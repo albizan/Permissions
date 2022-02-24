af262b58d74a91c44333549ae4e17167f7de845b
Alberto Zanotti <albizan@hotmail.it>
2022-02-24 18:11:41 +0100

    Added viewmodel

---
+++ ViewModels/RoleClaims.cs
@@ -0,0 +1,14 @@
+﻿namespace Permissions.ViewModels
+{
+    public class RoleClaims
+    {
+        public string Id { get; set; }
+        public IList<RoleClaim> Claims { get; set; }
+    }
+    public class RoleClaim
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
