d838eb34882cb5c8c7df830ced1f5ff9802f2502
Alberto Zanotti <albizan@hotmail.it>
2022-02-25 12:56:44 +0100

    Add model

---
+++ Models/Weapon.cs
@@ -0,0 +1,10 @@
+﻿namespace Permissions.Models
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int Damage { get; set; }
+        public int Level { get; set; }
+    }
+}
